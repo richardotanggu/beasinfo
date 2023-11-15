79a81d3d212499eeba2576cf73e9d5cd7cb8f17a
richardotanggu <richardotanggu@gmail.com>
2023-11-15 09:12:08 +0700

    perubahan di halaman login sama halaman info beasiswa

--- info.php
+++ info.php
@@ -1,109 +1,110 @@
 <?php
 include 'header.php';
-
 ?>
 
-  <main id="main" data-aos="fade-in">
+<main id="main" data-aos="fade-in">
 
     <!-- ======= Breadcrumbs ======= -->
     <div class="breadcrumbs">
-      <div class="container">
-        <h2>Info Beasiswa</h2>  
-      </div>
+        <div class="container">
+            <h2>Info Beasiswa</h2>
+        </div>
     </div><!-- End Breadcrumbs -->
 
     <!-- ======= Courses Section ======= -->
     <section id="courses" class="courses">
-      <div class="container" data-aos="fade-up">
+        <div class="container" data-aos="fade-up">
 
-        <div class="row" data-aos="zoom-in" data-aos-delay="100">
+            <div class="row" data-aos="zoom-in" data-aos-delay="100">
 
-          <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
-            <div class="course-item">
-              <img src="assets/img/course-1.jpg" class="img-fluid" alt="...">
-              <div class="course-content">
-                <div class="d-flex justify-content-between align-items-center mb-3">
-                  <h4>Web Development</h4>
-                  <p class="price">$169</p>
-                </div>
+                <!-- Beasiswa 1 -->
+                <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
+                    <div class="course-item">
+                        <img src="assets/img/beasiswa-1.jpg" class="img-fluid" alt="...">
+                        <div class="course-content">
+                            <div class="d-flex justify-content-between align-items-center mb-3">
+                                <h4>Beasiswa A</h4>
+                                <p class="price">$1000</p>
+                            </div>
 
-                <h3><a href="course-details.html">Website Design</a></h3>
-                <p>Et architecto provident deleniti facere repellat nobis iste. Id facere quia quae dolores dolorem tempore.</p>
-                <div class="trainer d-flex justify-content-between align-items-center">
-                  <div class="trainer-profile d-flex align-items-center">
-                    <img src="assets/img/trainers/trainer-1.jpg" class="img-fluid" alt="">
-                    <span>Antonio</span>
-                  </div>
-                  <div class="trainer-rank d-flex align-items-center">
-                    <i class="bx bx-user"></i>&nbsp;50
-                    &nbsp;&nbsp;
-                    <i class="bx bx-heart"></i>&nbsp;65
-                  </div>
-                </div>
-              </div>
+                            <h3><a href="beasiswa-details.html">Nama Beasiswa A</a></h3>
+                            <p>Deskripsi singkat mengenai Beasiswa A. Beasiswa ini diberikan kepada mahasiswa yang memenuhi syarat tertentu.</p>
+                            <div class="trainer d-flex justify-content-between align-items-center">
+                                <div class="trainer-profile d-flex align-items-center">
+                                    <img src="assets/img/beasiswa-logo-1.jpg" class="img-fluid" alt="">
+                                    <span>Pemberi Beasiswa A</span>
+                                </div>
+                                <div class="trainer-rank d-flex align-items-center">
+                                    <i class="bx bx-user"></i>&nbsp;50
+                                    &nbsp;&nbsp;
+                                    <i class="bx bx-heart"></i>&nbsp;65
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div> <!-- End Beasiswa Item 1 -->
+
+                <!-- Beasiswa 2 -->
+                <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0">
+                    <div class="course-item">
+                        <img src="assets/img/beasiswa-2.jpg" class="img-fluid" alt="...">
+                        <div class="course-content">
+                            <div class="d-flex justify-content-between align-items-center mb-3">
+                                <h4>Beasiswa B</h4>
+                                <p class="price">$800</p>
+                            </div>
+
+                            <h3><a href="beasiswa-details.html">Nama Beasiswa B</a></h3>
+                            <p>Deskripsi singkat mengenai Beasiswa B. Beasiswa ini diberikan kepada mahasiswa yang memenuhi syarat tertentu.</p>
+                            <div class="trainer d-flex justify-content-between align-items-center">
+                                <div class="trainer-profile d-flex align-items-center">
+                                    <img src="assets/img/beasiswa-logo-2.jpg" class="img-fluid" alt="">
+                                    <span>Pemberi Beasiswa B</span>
+                                </div>
+                                <div class="trainer-rank d-flex align-items-center">
+                                    <i class="bx bx-user"></i>&nbsp;35
+                                    &nbsp;&nbsp;
+                                    <i class="bx bx-heart"></i>&nbsp;42
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div> <!-- End Beasiswa Item 2 -->
+
+                <!-- Beasiswa 3 -->
+                <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-lg-0">
+                    <div class="course-item">
+                        <img src="assets/img/beasiswa-3.jpg" class="img-fluid" alt="...">
+                        <div class="course-content">
+                            <div class="d-flex justify-content-between align-items-center mb-3">
+                                <h4>Beasiswa C</h4>
+                                <p class="price">$1200</p>
+                            </div>
+
+                            <h3><a href="beasiswa-details.html">Nama Beasiswa C</a></h3>
+                            <p>Deskripsi singkat mengenai Beasiswa C. Beasiswa ini diberikan kepada mahasiswa yang memenuhi syarat tertentu.</p>
+                            <div class="trainer d-flex justify-content-between align-items-center">
+                                <div class="trainer-profile d-flex align-items-center">
+                                    <img src="../assets/img/about.jpg" class="img-fluid" alt="">
+                                    <span>Pemberi Beasiswa C</span>
+                                </div>
+                                <div class="trainer-rank d-flex align-items-center">
+                                    <i class="bx bx-user"></i>&nbsp;20
+                                    &nbsp;&nbsp;
+                                    <i class="bx bx-heart"></i>&nbsp;85
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div> <!-- End Beasiswa Item 3 -->
+
             </div>
-          </div> <!-- End Course Item-->
-
-          <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0">
-            <div class="course-item">
-              <img src="assets/img/course-2.jpg" class="img-fluid" alt="...">
-              <div class="course-content">
-                <div class="d-flex justify-content-between align-items-center mb-3">
-                  <h4>Marketing</h4>
-                  <p class="price">$250</p>
-                </div>
-
-                <h3><a href="course-details.html">Search Engine Optimization</a></h3>
-                <p>Et architecto provident deleniti facere repellat nobis iste. Id facere quia quae dolores dolorem tempore.</p>
-                <div class="trainer d-flex justify-content-between align-items-center">
-                  <div class="trainer-profile d-flex align-items-center">
-                    <img src="assets/img/trainers/trainer-2.jpg" class="img-fluid" alt="">
-                    <span>Lana</span>
-                  </div>
-                  <div class="trainer-rank d-flex align-items-center">
-                    <i class="bx bx-user"></i>&nbsp;35
-                    &nbsp;&nbsp;
-                    <i class="bx bx-heart"></i>&nbsp;42
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div> <!-- End Course Item-->
-
-          <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-lg-0">
-            <div class="course-item">
-              <img src="assets/img/course-3.jpg" class="img-fluid" alt="...">
-              <div class="course-content">
-                <div class="d-flex justify-content-between align-items-center mb-3">
-                  <h4>Content</h4>
-                  <p class="price">$180</p>
-                </div>
-
-                <h3><a href="course-details.html">Copywriting</a></h3>
-                <p>Et architecto provident deleniti facere repellat nobis iste. Id facere quia quae dolores dolorem tempore.</p>
-                <div class="trainer d-flex justify-content-between align-items-center">
-                  <div class="trainer-profile d-flex align-items-center">
-                    <img src="assets/img/trainers/trainer-3.jpg" class="img-fluid" alt="">
-                    <span>Brandon</span>
-                  </div>
-                  <div class="trainer-rank d-flex align-items-center">
-                    <i class="bx bx-user"></i>&nbsp;20
-                    &nbsp;&nbsp;
-                    <i class="bx bx-heart"></i>&nbsp;85
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div> <!-- End Course Item-->
 
         </div>
+    </section><!-- End Beasiswa Section -->
 
-      </div>
-    </section><!-- End Courses Section -->
-
-  </main><!-- End #main -->
+</main><!-- End #main -->
 
 <?php
 include 'footer.php';
-
 ?>
