5c25c36d154c6930a7d6b256527c9887ceb1891d
fauziacamelia <fauziacamelia@gmail.com>
2023-12-06 09:16:54 +0700

    dari iyong

--- detail-beasiswa3.php
+++ detail-beasiswa3.php
@@ -5,36 +5,23 @@ include 'header.php';
  <!-- ======= Info Beasiswa ======= -->
     
 <!-- ======= Info Beasiswa ======= -->
-<section id="about" class="about">
+<section id="about" class="about mt-5">
       <div class="container" data-aos="fade-up">
 
         <div class="row">
           <div class="col-lg-6 order-1 order-lg-2" data-aos="fade-left" data-aos-delay="100">
-            <img src="assets/img/beasiswa-logo-1.jpg" class="img-fluid" alt="">
+            <img src="img.beasiswa/pendaftaran3.png" class="img-fluid" alt="">
           </div>
           <div class="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 content">
             <h6></h6>
             <p class="fst-italic">
-            [BEASISWA BAZNAS KOTA CIMAHI]
-            Dengan mengharap ridha Allah swt, Baznas Kota Cimahi kembali membuka kesempatan kepada putra putri terbaik Kota Cimahi untuk memperoleh Beasiswa Pendidikan S-1 Tahun 2023.
+            ABOUT BINUS UNIVERSITY
             </p>
             <p class="fst-italic">
-              Persyaratan:
+            BINUS UNIVERSITY merupakan Perguruan Tinggi Indonesia berkelas dunia dengan akreditasi Unggul dari BAN-PT dan masuk kedalam Top 10 Universitas di Indonesia versi World University Rangking 2023 dan merupakan 5 stars University dari QS University Ranking. Tidak hanya membekali BINUSIAN dengan pengalaman belajar di kampus, juga diberikan pengalaman langsung di dunia industri. Komitmen ini diwujudkan melalui program (2+1)+1 dengan skema 2 tahun kuliah di kampus asal, 1 tahun kuliah di kampus BINUS di kota lain, dan 1 tahun mengambil salah satu dari 7 program enrichment, yaitu: Internship, Community Development, Research, Entrepreneurship, Study Abroad, Spesific Independent Study dan Further Study agar BINUSIAN lebih berkompenten dan siap bersaing dalam dunia profesional global sesuai bidang yang BINUSIAN tekuni dan impikan nantinya.
             </p>
-            <ul>
-              <li><i class="bi bi-check-circle"></i>Masuk ke beranda Terdapat beberapa menu yaitu,  </li>
-              <li><i class="bi bi-check-circle"></i>Info Beasiswa (Disini kita bisa melihat info-info seputar beasiswa dari berbagai kampus). </li>
-              <li><i class="bi bi-check-circle"></i>Bimbingan persiapan pendaftaran (Terdapat 3 elemen yaitu: persiapan dokumen, tes beasiswa, dan ada tes wawancara). </li>
-              <li><i class="bi bi-check-circle"></i>Event ( Di menu ini terdapat webinar atau seputar informasi - informasi berkaitan dengan zoom dari kampus - kampus yang mengadakan webinar tersebut). </li>
-              <li><i class="bi bi-check-circle"></i>Tentang (Berisi tentang seputar aplikasi atau website yang kami buat). </li> <br>
-              <li><i class="bi bi-check-circle"></i>Login( admin login untuk mengupdate info beasiswa yang sudah lama dan mempublikasikan yang baru, selain itu juga dapat mengupdate webinar). </li>
-
-
-            </ul>
-            <p>
-            Beasiswa Terbuka:<br>
-             Jangan lewatkan kesempatan ini untuk meraih gelar akademismu.<br>
-              Daftar sekarang!!
+            Daftarkan Diri Kamu Sekarang Juga!!
+            <a href="https://binus.ac.id/beasiswa2024/">Klik disini</a>
             </p>
 
           </div>
